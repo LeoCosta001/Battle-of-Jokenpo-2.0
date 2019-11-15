5a876acacdc4a7c22c23ece213542d93ab42924c
LeoCosta001 <leocosta.black@hotmail.com>
2019-11-15 15:39:39 -0300

    ATT delay e rotação da jogada da CPU

--- script/jokenpo.js
+++ script/jokenpo.js
@@ -12,7 +12,7 @@ const playerPlacar = document.querySelector("#scorePlayer");
 const CPUPlacar = document.querySelector("#scoreCPU");
 // Variaveis de Status
 let partida = "off";
-let playerChange = 0;
+let playerChange = "";
 let CPUChange = 0;
 // Variaveis de Pontuação
 let playerScore = 0
@@ -24,68 +24,96 @@ let jogadasCPU = ["pedra", "papel", "tesoura"]
 butJogar.addEventListener("click", t => {
     // Verificar se a partida está "ON" ou "OFF"
     if (partida == "off") {
+
         // Verificar a escolha de jogada do Player
         if (playerChange == 0) {
             consoleGame.innerHTML = "ERRO! Selecione uma jogada primeiro."
         } else {
-            // Calcular um número randômico para CPU de 1 até 3
-            CPUChange = Math.round(Math.random() * (jogadasCPU.length -1))+1
+            // Verificar a escolha de jogada da CPU
+            //Efeito de rotação
+            let i = 1;
+            let CPURotation = window.setInterval(function () {
+                // Resetar rotação quando chegar na ultima opção
+                if (i > jogadasCPU.length) {
+                    i = 1;
+                }
+                // Alterando imagem da CPU
+                if (i == 1) { imgCPU.src = "imagens/pedra.png" }
+                if (i == 2) { imgCPU.src = "imagens/papel.png" }
+                if (i == 3) { imgCPU.src = "imagens/tesoura.png" }
+                i = i + 1;
+            }, 100); // <---- Velocidade de Rotação
 
-            // Usuario escolhe Pedra
-            if (playerChange == "pedra" && CPUChange == 1) {
-                imgCPU.src = "imagens/pedra.png"
-                consoleGame.innerHTML = "EMPATE!"
-                consoleGameDescri.innerHTML = "Infelizmente as duas Pedras se quebraram."
-                playerScore = playerScore + 5
-                CPUScore = CPUScore + 5
-            }
-            if (playerChange == "pedra" && CPUChange == 2) {
-                imgCPU.src = "imagens/papel.png"
-                consoleGame.innerHTML = "DERROTA!"
-                consoleGameDescri.innerHTML = "A sua Pedra foi embrulhada pelo Papel."
-                CPUScore = CPUScore + 10
-            }
-            if (playerChange == "pedra" && CPUChange == 3) {
-                imgCPU.src = "imagens/tesoura.png"
-                consoleGame.innerHTML = "VITÓRIA!"
-                consoleGameDescri.innerHTML = "A sua Pedra quebrou a Tesoura."
-                playerScore = playerScore + 10
+            // Pausando rotação
+            window.setTimeout(function () {
+                clearInterval(CPURotation);
+                // Calcular um número de jogada randômico para CPU baseado na quantidade de escolhas da array "jogadasCPU"
+                CPUChange = Math.round(Math.random() * (jogadasCPU.length - 1)) + 1;
+                if (CPUChange == 1) { imgCPU.src = "imagens/pedra.png" }
+                if (CPUChange == 2) { imgCPU.src = "imagens/papel.png" }
+                if (CPUChange == 3) { imgCPU.src = "imagens/tesoura.png" }
+                // Usuario escolhe Pedra
+                if (playerChange == "pedra" && CPUChange == 1) {
+                    imgCPU.src = "imagens/pedra.png"
+                    consoleGame.innerHTML = "EMPATE!"
+                    consoleGameDescri.innerHTML = "Infelizmente as duas Pedras se quebraram."
+                    playerScore = playerScore + 5
+                    CPUScore = CPUScore + 5
+                }
+                if (playerChange == "pedra" && CPUChange == 2) {
+                    imgCPU.src = "imagens/papel.png"
+                    consoleGame.innerHTML = "DERROTA!"
+                    consoleGameDescri.innerHTML = "A sua Pedra foi embrulhada pelo Papel."
+                    CPUScore = CPUScore + 10
+                }
+                if (playerChange == "pedra" && CPUChange == 3) {
+                    imgCPU.src = "imagens/tesoura.png"
+                    consoleGame.innerHTML = "VITÓRIA!"
+                    consoleGameDescri.innerHTML = "A sua Pedra quebrou a Tesoura."
+                    playerScore = playerScore + 10
+                }
+                // Usuario escolhe Papel
+                if (playerChange == "papel" && CPUChange == 1) {
+                    imgCPU.src = "imagens/pedra.png"
+                    consoleGame.innerHTML = "VITÓRIA!"
+                    consoleGameDescri.innerHTML = "O seu Papel embrulhou a Pedra."
+                    playerScore = playerScore + 10
+                }
+                if (playerChange == "papel" && CPUChange == 2) {
+                    imgCPU.src = "imagens/papel.png"
+                    consoleGame.innerHTML = "EMPATE!"
+                    consoleGameDescri.innerHTML = "Os dois Papeis se rasgaram."
+                    playerScore = playerScore + 5
+                    CPUScore = CPUScore + 5
+                }
+                if (playerChange == "papel" && CPUChange == 3) {
+                    imgCPU.src = "imagens/tesoura.png"
+                    consoleGame.innerHTML = "DERROTA!"
+                    consoleGameDescri.innerHTML = "O seu Papel foi cortado pela Tesoura."
+                    CPUScore = CPUScore + 10
+                }
+                // Usuario escolhe Tesoura
+                if (playerChange == "tesoura" && CPUChange == 1) {
+                    imgCPU.src = "imagens/pedra.png"
+                    consoleGame.innerHTML = "DERROTA!"
+                    consoleGameDescri.innerHTML = "A sua Tesoura foi quebrada pela Pedra."
+                    CPUScore = CPUScore + 10
+                }
+                if (playerChange == "tesoura" && CPUChange == 2) {
+                    imgCPU.src = "imagens/papel.png";
+                    consoleGame.innerHTML = "VITÓRIA!"
+                    consoleGameDescri.innerHTML = "A sua Tesoura cortou o Papel."
+                    playerScore = playerScore + 10
+                }
+                if (playerChange == "tesoura" && CPUChange == 3) {
+                    imgCPU.src = "imagens/tesoura.png";
+                    consoleGame.innerHTML = "EMPATE!"
+                    consoleGameDescri.innerHTML = "As duas Tesouras se cortaram."
+                    playerScore = playerScore + 5
+                    CPUScore = CPUScore + 5
+                }
+            }, 2000); // <---- Tempo para pausar evento      
 
-            }
-            // Usuario escolhe Papel
-            if (playerChange == "papel" && CPUChange == 1) {
-                imgCPU.src = "imagens/pedra.png"
-                consoleGame.innerHTML = "VITÓRIA! O seu Papel embrulhou a Pedra."
-                playerScore = playerScore + 10
-            }
-            if (playerChange == "papel" && CPUChange == 2) {
-                imgCPU.src = "imagens/papel.png"
-                consoleGame.innerHTML = "EMPATE! Os dois Papeis se rasgaram."
-                playerScore = playerScore + 5
-                CPUScore = CPUScore + 5
-            }
-            if (playerChange == "papel" && CPUChange == 3) {
-                imgCPU.src = "imagens/tesoura.png"
-                consoleGame.innerHTML = "DERROTA! O seu Papel foi cortado pela Tesoura."
-                CPUScore = CPUScore + 10
-            }
-            // Usuario escolhe Tesoura
-            if (playerChange == "tesoura" && CPUChange == 1) {
-                imgCPU.src = "imagens/pedra.png"
-                consoleGame.innerHTML = "DERROTA! A sua Tesoura foi quebrada pela Pedra.."
-                CPUScore = CPUScore + 10
-            }
-            if (playerChange == "tesoura" && CPUChange == 2) {
-                imgCPU.src = "imagens/papel.png";
-                consoleGame.innerHTML = "VITÓRIA! A sua Tesoura cortou o Papel."
-                playerScore = playerScore + 10
-            }
-            if (playerChange == "tesoura" && CPUChange == 3) {
-                imgCPU.src = "imagens/tesoura.png";
-                consoleGame.innerHTML = "EMPATE! As duas Tesouras se cortaram."
-                playerScore = playerScore + 5
-                CPUScore = CPUScore + 5
-            }
             partida = "on";
             butJogar.value = "Outra Partida";
             playerPlacar.innerHTML = `${playerScore}`
